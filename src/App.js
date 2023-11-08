444648b8213cd3d632bbb29b348a16c1e3799d86
Adrianodosanjoos <adriano.luizysa@gmail.com>
2023-11-08 20:50:34 -0300

    adiçao de novos codigos

--- src/App.js
+++ src/App.js
@@ -1,32 +1,64 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 
-import { Container, H1, Image, ContainerItens, InputLabel, Input, Button } from './styles'
+import axios from 'axios'
+
+import { Container, H1, Image, ContainerItens, InputLabel, Input, Button, User } from './styles'
 
 import People from './assets/people.svg'
 
 import Arrow from './assets/arrow.svg'
 
+import trash from './assets/trash.svg'
+
 
 
 function App() {
+  const [users, setUsers] = useState([]);
+  const InputName = useRef();
+  const InputAge = useRef();
+
+
+  async function addNewUser() {
+    const data = await axios.post("http://localhost:3001/users, {name: InputName.current.value, age: InputAge.current.value}")
+    //setUsers([...users, { id: Math.random(), name: InputName.current.value, age: InputAge.current.value }])
+   console.log(data)
+
+  }
+
+  function deleteUser(userId) {
+    const newUsers = users.filter((user) => user.id !== userId);
+
+    setUsers(newUsers);
+  }
 
   return (
     <Container>
 
-      <Image alt="logo-imagem" src={People}/>
+      <Image alt="logo-imagem" src={People} />
 
       <ContainerItens>
         <H1>Hello</H1>
+
+
+        <InputLabel>Nome</InputLabel>
+        <Input ref={InputName} placehoder="Nome" />
+
+        <InputLabel>Idade</InputLabel>
+        <Input ref={InputAge} placehoder="Idade" />
+
+        <Button onClick={addNewUser}>Cadastrar <img alt="seta" src={Arrow} /></Button>
+
+
+        <ul>
+
+          {users.map((user) => (
+            <User key={user.id}>
+              <p>{user.name}</p> <p>{user.age}</p>
+              <button onClick={() => deleteUser(user.id)}><img src={trash} alt="Lata-de-lixo" /></button>
+            </User>
+          ))}
+        </ul>
       </ContainerItens>
-
-      <InputLabel>Nome</InputLabel>
-      <Input placehoder="Nome" />
-
-      <InputLabel>Idade</InputLabel>
-      <Input placehoder="Idade" />
-
-      <Button>Cadastrar <img alt="seta" src={Arrow}/></Button>
-
 
     </Container>
   );
--- src/styles.js
+++ src/styles.js
@@ -10,7 +10,7 @@ flex-direction: column;
 align-items: center;
 gap: 40px;
 
-height: 100vh;
+max-height: 100%
 `;
 
 export const Image = styled.img`
@@ -18,7 +18,7 @@ margin-top: 30px;
 `;
 
 export const ContainerItens = styled.div`
-background: linear-gradient(git remote add origin 
+background: linear-gradient( 
     rgba(255, 255, 255, 0.6) 0.84%,
     rgba(255, 255, 255, 0.6) 0.85%,
     rgba(255, 255, 255, 0.15) 100%
@@ -29,7 +29,7 @@ background: linear-gradient(git remote add origin
   display: flex;
   flex-direction: column;
 
-  height: 100vh;
+  max-height: 100%
 
 `;
 
@@ -108,5 +108,45 @@ display: flex;
 align-items: center;
 justify-content: center;
 gap: 20px;
+
+&:hover{
+  opacity: 0.8;
+}
+
+&:active{
+  opacity: 0.5;
+}
+
+`;
+
+export const User = styled.li `
+display: flex;
+justify-content: space-around;
+align-items: center;
+margin-top: 20px;
+
+background: rgba(255, 255, 255, 0.25);
+box-shadow: 0px, 4px, 4px rgba(0, 0, 0, 0.25);
+border-radius: 14px;
+
+width: 342px;
+height: 58px;
+outline:none;
+border: none;
+
+p {
+  font-style: normal;
+  font-weight: normal;
+  font-size: 20px;
+  line-height: 28px;
+
+  color: #ffffff;
+}
+
+button {
+  background: none;
+  border: none;
+  cursor: pointer;
+}
 `;
 
